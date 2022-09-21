6127d9b4cc8737eb22b9994fe3611a948ed0192e
MarceloMra <moreira.marcelo321@gmail.com>
2022-09-21 20:53:41 -0300

    Initial commit.

---
+++ src/main/java/com/example/StockPricesFinder/StockPricesFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.StockPricesFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPricesFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPricesFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StockPricesFinder/StockPricesFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StockPricesFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPricesFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
